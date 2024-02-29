4746309325e4989f40ddb10524c68fb19cdc330e
Mateus Ferro <mateus.ferro.2001@hotmail.com>
2024-02-29 11:34:24 -0300

    feat: adiciona um publisher em python

---
+++ application/publisher/publisher.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as mqtt
+
+topic = "/messages"
+payload = "Publisher Python File Sending Message"
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "publisher")
+
+mqtt_client.connect(host = "localhost", port = 1883)
+mqtt_client.publish(topic = topic, payload = payload)
+print(f"\nMessage published to topic '{topic}' with payload: {payload}\n")
